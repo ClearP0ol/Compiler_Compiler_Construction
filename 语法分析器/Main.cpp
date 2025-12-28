b016f58be898b48cee0f4ee73ab176f3669f5dad
JuanjuanCaier <179804316+JuanjuanCaier@users.noreply.github.com>
2025-12-28 19:55:50 +0800

    修改用词

--- 语法分析器/Main.cpp
+++ 语法分析器/Main.cpp
@@ -5,23 +5,22 @@ using namespace std;
 
 int main() {
 
-    // 创建文法加载器
+    // 创建语法加载器
     GrammarLoader Loader;
 
     // 测试文件路径
     string GrammarFile = "MiniC.grammar";
 
-    // 加载文法
-    cout << "正在加载文法文件: " << GrammarFile << endl;
+    // 加载语法
     GrammarDefinition Grammar = Loader.LoadFromFile(GrammarFile);
 
     // 检查是否加载成功
     if (Grammar.Productions.empty()) {
-        cout << endl << "文法加载失败！" << endl;
+        cout << endl << "语法加载失败！" << endl;
         return 1;
     }
 
-    cout << endl << "文法加载成功！" << endl;
+    cout << endl << "语法加载成功！" << endl;
 
    
     // 创建FIRST/FOLLOW计算器
